19608fac010d068a2179713d493d6dff063be7a8
ParkHyebeen <70693435+ParkHyebeen@users.noreply.github.com>
2021-07-02 10:18:10 +0900

    1 added 4 modified

--- app/src/main/java/com/example/camppractice3/MainActivity.java
+++ app/src/main/java/com/example/camppractice3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.camppractice3;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,44 +8,75 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1, b2, b3;
+    EditText e1, e2;
+    TextView tv1;
 
-    Button b1, b2;
-    EditText et1;
+    private final static int REQUEST_CODE_1 = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1=findViewById(R.id.btnCall);
-        b1.setOnClickListener(new View.OnClickListener(){
+        b1= findViewById(R.id.btnCall);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                callMainActivity2();
+            public void onClick(View v) {
+                callSecondActivity();
             }
         });
 
-        b2 =findViewById(R.id.btnMsg);
-        et1 = findViewById(R.id.etMsg);
-        b2.setOnClickListener(new View.OnClickListener(){
+
+        b2 = findViewById(R.id.btnMsg);
+        e1 = findViewById(R.id.etMsg);
+        b2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                callMainActivity2WithMsg();
+            public void onClick(View v) {
+                callSecondActivityWithMsg();
             }
         });
+
+        b3 = findViewById(R.id.btnBoth);
+        e2 = findViewById(R.id.etMsg2);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callSecondActivityWithMsg3();
+            }
+        });
+
     }
 
-    private void callMainActivity2(){
-        //명시적 intent
-        Intent intent = new Intent(this,MainActivity2.class);
-        startActivity(intent);
-    }
-
-    private void callMainActivity2WithMsg(){
+    private void callSecondActivityWithMsg3() {
         Intent intent = new Intent(this, MainActivity2.class);
-        intent.putExtra("msg",et1.getText().toString());
+        intent.putExtra("msg", e2.getText().toString());
+        startActivityForResult(intent, REQUEST_CODE_1);
+    }
+
+    private void callSecondActivityWithMsg() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("msg", e1.getText().toString());
         startActivity(intent);
+    }
+
+    private void callSecondActivity() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE_1){
+            if(resultCode == RESULT_OK){
+                String msg = data.getStringExtra("msg");
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/camppractice3/MainActivity2.java
+++ app/src/main/java/com/example/camppractice3/MainActivity2.java
@@ -1,25 +1,28 @@
 package com.example.camppractice3;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
-    Button b1;
+    Button b1, b2;
     TextView tv1;
+    EditText e1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        init();
+    }
 
-        b1=findViewById(R.id.btnClose);
-        b1.setOnClickListener(new View.OnClickListener(){
+    private void init(){
+        b1 = findViewById(R.id.btnClose);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 callClose();
@@ -27,14 +30,30 @@ public class MainActivity2 extends AppCompatActivity {
         });
 
 
-        tv1= findViewById(R.id.tvMsg);
+        tv1 = findViewById(R.id.tvMsg);
         Intent intent = getIntent();
         String msg = intent.getStringExtra("msg");
         tv1.setText(msg);
 
+
+        b2 = findViewById(R.id.btnMsg);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callSendMsg();
+            }
+        });
+        e1 =findViewById(R.id.etMsg);
+
+    }
+    private void callSendMsg() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("msg", e1.getText().toString());
+        setResult(RESULT_OK,intent);
+        finish();
     }
 
-    private void callClose(){
+    private void callClose() {
         finish();
     }
 }
